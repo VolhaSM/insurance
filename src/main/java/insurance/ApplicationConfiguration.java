e494686f3399c534c52064a697b47662f4c59e2a
Volha Miadzvetskaya <volha.sm@gmail.com>
2020-12-21 00:14:09 +0300

    JSON

--- src/main/java/insurance/ApplicationConfiguration.java
+++ src/main/java/insurance/ApplicationConfiguration.java
@@ -12,23 +12,6 @@ import org.springframework.context.annotation.Configuration;
 //@EnableWebSecurity
 public class ApplicationConfiguration   {
 
-    //extends WebSecurityConfigurerAdapter
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication()
-//                .withUser("user")
-//                .password("{noop}password")
-//                .roles("USER");
-//    }
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.httpBasic().and()
-//                .authorizeRequests().antMatchers("/**").hasRole("USER")
-//                .and().formLogin().disable()
-//                .csrf().disable();
-//    }
-
     public static void main(String[] args) {
 
         SpringApplication.run(ApplicationConfiguration.class);
